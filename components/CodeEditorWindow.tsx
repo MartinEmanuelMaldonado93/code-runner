1a0c1195ac7d9f590c9323375e01c254df37de1a
Martin Emanuel <martinemanuel93@gmail.com>
2023-01-06 15:14:39 -0300

    add new code editor

---
+++ components/CodeEditorWindow.tsx
@@ -0,0 +1,33 @@
+import { Dispatch, SetStateAction, useCallback, useMemo, useState } from 'react';
+import Editor, { EditorProps } from "@monaco-editor/react";
+
+interface props extends EditorProps {
+  code?:string,
+}
+const CodeEditorWindow:React.FC<props> = ({ onChange, language, theme, code }) => {
+  const [valueCode, setValue] = useState(" ");
+
+  const handleEditorChange = (value: string | undefined) => {
+    value ??= "";
+    console.log(value);
+    setValue(value);
+    onChange!("code", value);
+  };
+
+  return (
+    <div className="overlay rounded-md overflow-hidden w-full h-full shadow-4xl">
+      <Editor
+        height="85vh"
+        width={`100%`}
+        language={language || "javascript"}
+        value={valueCode}
+        theme={theme}
+        defaultValue="// some comment"
+        onChange={
+          handleEditorChange
+        }
+      />
+    </div>
+  );
+};
+export default CodeEditorWindow;
---
+++ pages/api/martin.ts
@@ -0,0 +1,21 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import type { NextApiRequest, NextApiResponse } from 'next'
+import data from "./martin.json";
+
+// console.log("api", data);// backend
+
+// type Data = {
+//   name: unknown
+// }
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse 
+) {
+  // const { userName } = req.query ;
+  // res.status(200).json([{ name: userName },])
+  // fetch("./martin.json")
+  // .then(console.log)
+  res.status(200).json(data)
+}
