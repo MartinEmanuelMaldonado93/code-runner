fdc1ad77399159f2a862ceeaed9e7151e4121182
Martin Emanuel <martinemanuel93@gmail.com>
2023-03-08 16:43:00 -0300

    uncomment env calls

--- api/axiosLib/axiosInstance.ts
+++ api/axiosLib/axiosInstance.ts
@@ -1,11 +1,11 @@
 import Axios from "axios";
 import { addBaseInterceptors } from "./axiosInterceptors";
 
-// if (!process.env.APP_RAPID_API_KEY) throw Error("APP_RAPID_API_KEY not defined");
-// if (!process.env.NEXT_PUBLIC_APP_RAPID_API_BASE_URL)
-//   throw Error("API_URL not defined");
-// if (!process.env.NEXT_PUBLIC_APP_RAPID_API_HOST)
-//   throw Error("API_HOST not defined");
+if (!process.env.APP_RAPID_API_KEY) throw Error("APP_RAPID_API_KEY not defined");
+if (!process.env.NEXT_PUBLIC_APP_RAPID_API_BASE_URL)
+  throw Error("API_URL not defined");
+if (!process.env.NEXT_PUBLIC_APP_RAPID_API_HOST)
+  throw Error("API_HOST not defined");
 
 export const judge0Instance = Axios.create({
   baseURL: process.env.NEXT_PUBLIC_APP_RAPID_API_BASE_URL,
